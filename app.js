f80bc5377136dca404dc9b69fa17973812aef3fe
Cristal Chavez <cristalmchavez@yahoo.com>
2015-04-30 21:24:28 -0700

    button prints string to console

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ wdi_16_names = ["Alexander Mireles", "Andre Trelles", "Andrew Ham", "Ari Kanamor
 // console.log(namePicker);
 // var namePicker = wdi_16_names[Math.floor(Math.random() * wdi_16_names.length)];
 // console.log(namePicker);
-function pickRandom(){
-	var namePicker = wdi_16_names[Math.floor(Math.random() * wdi_16_names.length)];
-	document.write(namePicker); 
-}
 
+// function pickRandom(){
+// 	var namePicker = wdi_16_names[Math.floor(Math.random() * wdi_16_names.length)];
+// 	document.getElementById('random').innerHTML = namePicker
+// }
+
+function namePick() { 
+	return Math.floor(Math.random() * wdi_16_names.length);}
+
+function eachName() {
+	document.getElementById("random").innerHTML = wdi_16_names.splice(namePick(),1);
+	console.log(wdi_16_names.length)
+	if (wdi_16_names.length == 0) {
+		wdi_16_names = 	["Alexander Mireles", "Andre Trelles", "Andrew Ham", "Ari Kanamori", "Busyra Anwar", "Carlos Meza", "Chris Garvin", "Cristal Chavez", "Daley Meistrell", "Daniel Manuel", "Jefferson Choi", "Jessica Grinberg", "John Ward", "Kate Wood", "Kevin Mehrabi", "Max Tran", "Max Plisskin", "Megan Kakitsubo", "Meryl Harden", "Mike McCready", "Nick Dalke","Niels Bischoff", "Paul Aglione", "Rahul Sidhu", "Samuel Chun", "Samuel Amiri", "Sophie Chor","Steve Soter", "Taylor Adams", "Yo Wakita"];
+	}
+}
